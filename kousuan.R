6e6b72423ffba401ec3486f97934d0ece9da8606
Tian Shixiong <tiansworld@fedoraproject.org>
2022-08-30 09:52:16 +0800

    Export wrong or skipped formulas to file.
    This is the default behaviour, and the file is saved under R
    working directory, named 'cuoti.csv'. The formulas will be
    appended to the file each time, so the previous formulas
    won't be erased.
    
    Minor changes:
    1. Improve the calculation of correctness, ignore skipped
    formulas.
    2. Change the meaning of 'length', '1' means numbers between
    0 and 20, '2' means numbers between 10 and 50, if 'length' is
    ignored, then numbers are between 0 and 50.
    3. Add the 4th column '正误' in the result data frame, which
    makes the wrong results be identified easier.

--- kousuan.R
+++ kousuan.R
@@ -1,7 +1,7 @@
 ## Arithmetic exercise for 1st grade student
 ## Copyright 2022   Tian Shixiong
 
-#' @length  生成数字的位数，1 为一位数, 2 为两位数
+#' @length  生成数字的大小，1 为 0-20，2 为 10-50，不指定为 0-50
 #' @size    生成数字的个数
 #' @num     组成算式的数字个数
 #' @count   算式的数量
@@ -14,14 +14,14 @@
 # draw numbers
 draw_number <- function(length, size) { 
     if(length >= 3 | length < 0) {
-        print("仅限 1 位或 2 位数，请输入 1 或 2.")
+        print("请输入 1 或 2.")
         stop()
     } else if(length == 2) {
-        sample(10:99, replace = TRUE, size = size)
+        sample(10:50, replace = TRUE, size = size)
     } else if (length == 1) {
-        sample(0:9, replace = TRUE, size = size)
+        sample(0:20, replace = TRUE, size = size)
     } else {
-        sample(0:99, replace = TRUE, size = size)
+        sample(0:50, replace = TRUE, size = size)
     }
 }
 
@@ -48,15 +48,22 @@ play <- function(count = 10, num = 2, length = 1) {
         d <- rbind(d, 
                    data.frame("算式" = e, 
                               "结果" = as.numeric(a),
-                              "正确答案" = eval(parse(text=e))
+                              "正确答案" = eval(parse(text=e)),
+                              "正误" = NA
                               ))
 }
+    d[,4] <- d[,2] == d[,3]
+    cuoti <- d[d[,4] == FALSE | is.na(d[,4]), 1]
     t1 <- Sys.time()
     s <- round(as.numeric(difftime(t1,t, units = "secs")),1)
     s <- paste0("用时：", s, "秒", collapse="")
-    p <- paste0(mean(d[2] == d[3]) * 100, "%")
-# statistics
-    # how to transform character strings to equation and calculate it?
+    p <- paste0(mean(d[2] == d[3], na.rm = TRUE) * 100, "%")
+    write.table(cuoti,
+              file = "./cuoti.csv", 
+              append = TRUE,
+              col.names = FALSE,
+              sep = "\t")
+    # statistics
     return(list("结果" = d, "时间" = s, "正确率" = p))
 }
 
